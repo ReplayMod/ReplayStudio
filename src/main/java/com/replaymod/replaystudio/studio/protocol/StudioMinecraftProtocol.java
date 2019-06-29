ReplayMod/ReplayStudio

commit 4522c9966fbb3f50b68c28a4cc39ed2a5ad0e7a5

Stop setting final fields with reflection
This has always been a hack and with Java 12 it has finally stopped
working.

src/main/java/com/replaymod/replaystudio/studio/protocol/StudioMinecraftProtocol.java
@@ -27,10 +27,8 @@ package com.replaymod.replaystudio.studio.protocol;
 import com.github.steveice10.mc.protocol.MinecraftProtocol;
 import com.github.steveice10.packetlib.Session;
 import com.github.steveice10.packetlib.packet.Packet;
-import com.github.steveice10.packetlib.packet.PacketProtocol;
 import com.replaymod.replaystudio.Studio;
 import com.replaymod.replaystudio.io.WrappedPacket;
-import com.replaymod.replaystudio.util.Reflection;
 
 //#if MC>=10800
 import com.github.steveice10.mc.protocol.data.SubProtocol;
@@ -39,16 +37,11 @@ import com.github.steveice10.mc.protocol.data.SubProtocol;
 //#endif
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class StudioMinecraftProtocol extends MinecraftProtocol {
 
-    public StudioMinecraftProtocol() {
-        //#if MC>=10800
-        super(SubProtocol.LOGIN);
-        //#else
-        //$$ super(ProtocolMode.LOGIN);
-        //#endif
-    }
+    private final Studio studio;
 
     /**
      * @deprecated Use {@link #StudioMinecraftProtocol(Studio, Session, boolean, boolean)} instead.
@@ -65,50 +58,45 @@ public class StudioMinecraftProtocol extends MinecraftProtocol {
         //$$ super(ProtocolMode.LOGIN);
         //#endif
 
-        init(studio, session, client,
-                //#if MC>=10800
-                includeLoginPhase ? SubProtocol.LOGIN : SubProtocol.GAME
-                //#else
-                //$$ includeLoginPhase ? ProtocolMode.LOGIN : ProtocolMode.GAME
-                //#endif
-        );
+        this.studio = studio;
+
+        //#if MC>=10800
+        setSubProtocol(includeLoginPhase ? SubProtocol.LOGIN : SubProtocol.GAME, client, session);
+        //#else
+        //$$ setMode(includeLoginPhase ? ProtocolMode.LOGIN : ProtocolMode.GAME, client, session);
+        //#endif
     }
 
-    //#if MC>=10800
-    public void init(Studio studio, Session session, boolean client, SubProtocol mode) {
-    //#else
-    //$$ public void init(Studio studio, Session session, boolean client, ProtocolMode mode) {
-    //#endif
-        Reflection.setField(PacketProtocol.class, "incoming", this, new HashMap() {
+    @Override
+    protected Map<Integer, Class<? extends Packet>> createIncomingMap() {
+        return new HashMap<Integer, Class<? extends Packet>>() {
             @Override
-            @SuppressWarnings("unchecked")
-            public Object get(Object key) {
-                Class<? extends Packet> value = (Class<? extends Packet>) super.get(key);
-                return getPacketClass(studio, value);
+            public Class<? extends Packet> get(Object key) {
+                return getPacketClass(studio, super.get(key));
             }
-        });
+        };
+    }
 
-        Reflection.setField(PacketProtocol.class, "outgoing", this, new HashMap() {
+    @Override
+    protected Map<Class<? extends Packet>, Integer> createOutgoingMap() {
+        return new HashMap<Class<? extends Packet>, Integer>() {
             @Override
             public boolean containsKey(Object key) {
                 return get(key) != null;
             }
 
             @Override
-            @SuppressWarnings("unchecked")
-            public Object get(Object key) {
+            public Integer get(Object key) {
                 if (!(key instanceof Class)) {
                     return super.get(key);
                 }
-                return super.get(WrappedPacket.getWrappedClassFor((Class) key));
+                Class<?> cls = (Class<?>) key;
+                if (!cls.isAssignableFrom(Packet.class)) {
+                    return super.get(key);
+                }
+                return super.get(WrappedPacket.getWrappedClassFor(cls.asSubclass(Packet.class)));
             }
-        });
-
-        //#if MC>=10800
-        setSubProtocol(mode, client, session);
-        //#else
-        //$$ setMode(mode, client, session);
-        //#endif
+        };
     }
 
     //#if MC>=10800
@@ -123,7 +111,7 @@ public class StudioMinecraftProtocol extends MinecraftProtocol {
     //$$ }
     //#endif
 
-    private Class<?> getPacketClass(Studio studio, Class<? extends Packet> cls) {
+    private Class<? extends Packet> getPacketClass(Studio studio, Class<? extends Packet> cls) {
         if (studio.isWrappingEnabled() && !studio.willBeParsed(cls)) {
             return WrappedPacket.getClassFor(cls);
         } else {

src/main/java/com/github/steveice10/packetlib/packet/PacketProtocol.java
@@ -1,0 +1,169 @@
+/*
+ * This is a 1:1 copy of the class from PacketLib except with the option to supply a custom map for `incoming` and `outgoing`.
+ * We used to do this via reflection but modifying final fields was finally made impossible with JDK12 (at least the way we did it).
+ * https://github.com/Steveice10/PacketLib/blob/1d5a0ad81bfd99325a1db9991ced09ab773f55c8/src/main/java/com/github/steveice10/packetlib/packet/PacketProtocol.java
+ *
+ * Original copyright notice:
+ *
+ * Copyright (C) 2013-2018 Steveice10
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.github.steveice10.packetlib.packet;
+
+import com.github.steveice10.packetlib.Client;
+import com.github.steveice10.packetlib.Server;
+import com.github.steveice10.packetlib.Session;
+import com.github.steveice10.packetlib.crypt.PacketEncryption;
+
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A protocol for packet sending and receiving.
+ * All implementations must have a no-params constructor for server protocol creation.
+ */
+public abstract class PacketProtocol {
+    private final Map<Integer, Class<? extends Packet>> incoming = createIncomingMap();
+    private final Map<Class<? extends Packet>, Integer> outgoing = createOutgoingMap();
+
+    protected Map<Integer, Class<? extends Packet>> createIncomingMap() {
+        return new HashMap<>();
+    }
+
+    protected Map<Class<? extends Packet>, Integer> createOutgoingMap() {
+        return new HashMap<>();
+    }
+
+    /**
+     * Gets the prefix used when locating SRV records for this protocol.
+     *
+     * @return The protocol's SRV record prefix.
+     */
+    public abstract String getSRVRecordPrefix();
+
+    /**
+     * Gets the packet header of this protocol.
+     *
+     * @return The protocol's packet header.
+     */
+    public abstract PacketHeader getPacketHeader();
+
+    /**
+     * Gets this protocol's active packet encryption.
+     *
+     * @return The protocol's packet encryption, or null if packets should not be encrypted.
+     */
+    public abstract PacketEncryption getEncryption();
+
+    /**
+     * Called when a client session is created with this protocol.
+     *
+     * @param client  The client that the session belongs to.
+     * @param session The created session.
+     */
+    public abstract void newClientSession(Client client, Session session);
+
+    /**
+     * Called when a server session is created with this protocol.
+     *
+     * @param server  The server that the session belongs to.
+     * @param session The created session.
+     */
+    public abstract void newServerSession(Server server, Session session);
+
+    /**
+     * Clears all currently registered packets.
+     */
+    public final void clearPackets() {
+        this.incoming.clear();
+        this.outgoing.clear();
+    }
+
+    /**
+     * Registers a packet to this protocol as both incoming and outgoing.
+     *
+     * @param id     Id to register the packet to.
+     * @param packet Packet to register.
+     * @throws IllegalArgumentException If the packet fails a test creation when being registered as incoming.
+     */
+    public final void register(int id, Class<? extends Packet> packet) {
+        this.registerIncoming(id, packet);
+        this.registerOutgoing(id, packet);
+    }
+
+    /**
+     * Registers an incoming packet to this protocol.
+     *
+     * @param id     Id to register the packet to.
+     * @param packet Packet to register.
+     * @throws IllegalArgumentException If the packet fails a test creation.
+     */
+    public final void registerIncoming(int id, Class<? extends Packet> packet) {
+        this.incoming.put(id, packet);
+        try {
+            this.createIncomingPacket(id);
+        } catch(IllegalStateException e) {
+            this.incoming.remove(id);
+            throw new IllegalArgumentException(e.getMessage(), e.getCause());
+        }
+    }
+
+    /**
+     * Registers an outgoing packet to this protocol.
+     *
+     * @param id     Id to register the packet to.
+     * @param packet Packet to register.
+     */
+    public final void registerOutgoing(int id, Class<? extends Packet> packet) {
+        this.outgoing.put(packet, id);
+    }
+
+    /**
+     * Creates a new instance of an incoming packet with the given id.
+     *
+     * @param id Id of the packet to create.
+     * @return The created packet.
+     * @throws IllegalArgumentException If the packet ID is invalid.
+     * @throws IllegalStateException    If the packet does not have a no-params constructor or cannot be instantiated.
+     */
+    public final Packet createIncomingPacket(int id) {
+        if(id < 0 || !this.incoming.containsKey(id) || this.incoming.get(id) == null) {
+            throw new IllegalArgumentException("Invalid packet id: " + id);
+        }
+
+        Class<? extends Packet> packet = this.incoming.get(id);
+        try {
+            Constructor<? extends Packet> constructor = packet.getDeclaredConstructor();
+            if(!constructor.isAccessible()) {
+                constructor.setAccessible(true);
+            }
+
+            return constructor.newInstance();
+        } catch(NoSuchMethodError e) {
+            throw new IllegalStateException("Packet \"" + id + ", " + packet.getName() + "\" does not have a no-params constructor for instantiation.");
+        } catch(Exception e) {
+            throw new IllegalStateException("Failed to instantiate packet \"" + id + ", " + packet.getName() + "\".", e);
+        }
+    }
+
+    /**
+     * Gets the registered id of an outgoing packet class.
+     *
+     * @param packet Class of the packet to get the id for.
+     * @return The packet's registered id.
+     * @throws IllegalArgumentException If the packet is not registered.
+     */
+    public final int getOutgoingId(Class<? extends Packet> packet) {
+        if(!this.outgoing.containsKey(packet) || this.outgoing.get(packet) == null) {
+            throw new IllegalArgumentException("Unregistered outgoing packet class: " + packet.getName());
+        }
+
+        return this.outgoing.get(packet);
+    }
+}

src/main/java/com/replaymod/replaystudio/io/WrappedPacket.java
@@ -78,8 +78,7 @@ public class WrappedPacket implements IWrappedPacket {
      * @param wrapper The wrapper whose packet class to get
      * @return The packet class or the specified class if it is no wrapper class
      */
-    @SuppressWarnings("unchecked")
-    public static synchronized Class<? extends Packet> getWrappedClassFor(Class<? extends IWrappedPacket> wrapper) {
+    public static synchronized Class<? extends Packet> getWrappedClassFor(Class<? extends Packet> wrapper) {
         ClassLoader loader = wrapper.getClassLoader();
         return loader instanceof WrappingClassLoader ? ((WrappingClassLoader) loader).wrapped : wrapper;
     }
@@ -90,7 +89,6 @@ public class WrappedPacket implements IWrappedPacket {
      * @param wrapper The packet
      * @return The real class
      */
-    @SuppressWarnings("unchecked")
     public static Class<? extends Packet> getWrapped(Packet wrapper) {
         return wrapper instanceof IWrappedPacket ? ((IWrappedPacket) wrapper).getWrapped() : wrapper.getClass();
     }

src/main/java/com/replaymod/replaystudio/util/Reflection.java
@@ -1,59 +1,0 @@
-/*
- * This file is part of ReplayStudio, licensed under the MIT License (MIT).
- *
- * Copyright (c) 2016 johni0702 <https://github.com/johni0702>
- * Copyright (c) contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-package com.replaymod.replaystudio.util;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-/**
- * Reflection utilities.
- */
-public class Reflection {
-
-    /**
-     * Sets a private final field to the new value
-     * @param cls The class which declares the field
-     * @param fieldName The name of the field
-     * @param obj The object or {@code null} if the field is static
-     * @param value The new value to set the field to
-     */
-    public static void setField(Class<?> cls, String fieldName, Object obj, Object value) {
-        try {
-            Field field = cls.getDeclaredField(fieldName);
-            field.setAccessible(true);
-
-            if ((field.getModifiers() & Modifier.FINAL) != 0) {
-                Field modifiersField = Field.class.getDeclaredField("modifiers");
-                modifiersField.setAccessible(true);
-                modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-            }
-
-            field.set(obj, value);
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-}

src/test/java/com/replaymod/replaystudio/mock/MinecraftProtocolMock.java
@@ -1,127 +1,0 @@
-/*
- * This file is part of ReplayStudio, licensed under the MIT License (MIT).
- *
- * Copyright (c) 2016 johni0702 <https://github.com/johni0702>
- * Copyright (c) contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-package com.replaymod.replaystudio.mock;
-
-import com.github.steveice10.mc.protocol.MinecraftProtocol;
-import com.github.steveice10.packetlib.Session;
-import com.github.steveice10.packetlib.packet.Packet;
-import com.github.steveice10.packetlib.packet.PacketProtocol;
-import com.google.common.base.Function;
-import com.replaymod.replaystudio.util.Reflection;
-
-//#if MC>=10800
-import com.github.steveice10.mc.protocol.data.SubProtocol;
-//#else
-//$$ import com.github.steveice10.mc.protocol.ProtocolMode;
-//#endif
-
-import java.lang.reflect.Constructor;
-import java.util.HashMap;
-
-public class MinecraftProtocolMock extends MinecraftProtocol {
-
-    private Function<Class<? extends Packet>, Class<? extends Packet>> incoming;
-    private Function<Class<? extends Packet>, Class<? extends Packet>> outgoing;
-
-    public MinecraftProtocolMock(Session session, boolean client,
-                                 Function<Class<? extends Packet>, Class<? extends Packet>> incoming,
-                                 Function<Class<? extends Packet>, Class<? extends Packet>> outgoing) {
-        //#if MC>=10800
-        super(SubProtocol.LOGIN);
-        //#else
-        //$$ super(ProtocolMode.LOGIN);
-        //#endif
-
-        this.incoming = incoming;
-        this.outgoing = outgoing;
-
-        //#if MC>=10800
-        init(session, client, SubProtocol.GAME);
-        //#else
-        //$$ init(session, client, ProtocolMode.GAME);
-        //#endif
-    }
-
-    //#if MC>=10800
-    public void init(Session session, boolean client, SubProtocol mode) {
-    //#else
-    //$$ public void init(Session session, boolean client, ProtocolMode mode) {
-    //#endif
-        Reflection.setField(PacketProtocol.class, "incoming", this, new HashMap() {
-            @Override
-            @SuppressWarnings("unchecked")
-            public Object put(Object key, Object value) {
-                if (value instanceof Constructor) {
-                    Constructor constructor = (Constructor) value;
-                    Class<? extends Packet> cls = constructor.getDeclaringClass();
-                    cls = incoming.apply(cls);
-                    if (cls != null) {
-                        try {
-                            constructor = cls.getDeclaredConstructor();
-                        } catch (NoSuchMethodException e) {
-                            throw new RuntimeException(e);
-                        }
-                        constructor.setAccessible(true);
-                    }
-                    value = constructor;
-                }
-                return super.put(key, value);
-            }
-        });
-
-        Reflection.setField(PacketProtocol.class, "outgoing", this, new HashMap() {
-            @Override
-            @SuppressWarnings("unchecked")
-            public Object put(Object key, Object value) {
-                if (key instanceof Class) {
-                    Class<? extends Packet> cls = (Class) key;
-                    cls = outgoing.apply(cls);
-                    if (cls != null) {
-                        key = cls;
-                    }
-                }
-                return super.put(key, value);
-            }
-        });
-
-        //#if MC>=10800
-        setSubProtocol(mode, client, session);
-        //#else
-        //$$ setMode(mode, client, session);
-        //#endif
-    }
-
-    //#if MC>=10800
-    @Override
-    public void setSubProtocol(SubProtocol mode, boolean client, Session session) {
-        super.setSubProtocol(mode, client, session);
-    }
-    //#else
-    //$$ @Override
-    //$$ public void setMode(ProtocolMode mode, boolean client, Session session) {
-    //$$     super.setMode(mode, client, session);
-    //$$ }
-    //#endif
-}

versions/common.gradle
@@ -112,6 +112,7 @@ license {
     }
     exclude 'LICENSE.txt'
     exclude 'META-INF/*'
+    exclude 'com/github/steveice10/packetlib/packet/PacketProtocol.java'
 }
 
 preshadowJar {

commit 7ca86e1564302aed11436665ce4a385cd492c850

Fix logic error in packet wrapping introduced in 4522c99

src/main/java/com/replaymod/replaystudio/studio/protocol/StudioMinecraftProtocol.java
@@ -91,7 +91,7 @@ public class StudioMinecraftProtocol extends MinecraftProtocol {
                     return super.get(key);
                 }
                 Class<?> cls = (Class<?>) key;
-                if (!cls.isAssignableFrom(Packet.class)) {
+                if (!Packet.class.isAssignableFrom(cls)) {
                     return super.get(key);
                 }
                 return super.get(WrappedPacket.getWrappedClassFor(cls.asSubclass(Packet.class)));