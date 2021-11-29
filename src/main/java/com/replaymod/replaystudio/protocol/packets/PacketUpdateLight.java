ReplayMod/ReplayStudio

commit a5a92b65d5374a8587d241fb3dc50aec2be0fa20

Update to 1.18-pre1

src/main/java/com/replaymod/replaystudio/protocol/packets/PacketUpdateLight.java
@@ -34,8 +34,7 @@ public class PacketUpdateLight {
     private static final byte[] EMPTY = new byte[2048];
     private int x;
     private int z;
-    private List<byte[]> skyLight;
-    private List<byte[]> blockLight;
+    private Data data;
 
     public static PacketUpdateLight read(Packet packet) throws IOException {
         if (packet.getType() != PacketType.UpdateLight) {
@@ -60,10 +59,13 @@ public class PacketUpdateLight {
     }
 
     public PacketUpdateLight(int x, int z, List<byte[]> skyLight, List<byte[]> blockLight) {
+        this(x, z, new Data(skyLight, blockLight));
+    }
+
+    public PacketUpdateLight(int x, int z, Data data) {
         this.x = x;
         this.z = z;
-        this.skyLight = skyLight;
-        this.blockLight = blockLight;
+        this.data = data;
     }
 
     public int getX() {
@@ -74,17 +76,27 @@ public class PacketUpdateLight {
         return this.z;
     }
 
+    public Data getData() {
+        return this.data;
+    }
+
     public List<byte[]> getSkyLight() {
-        return this.skyLight;
+        return this.data.skyLight;
     }
 
     public List<byte[]> getBlockLight() {
-        return this.blockLight;
+        return this.data.blockLight;
     }
 
     private void read(Packet packet, Packet.Reader in) throws IOException {
         this.x = in.readVarInt();
         this.z = in.readVarInt();
+        this.data = readData(packet, in);
+    }
+
+    static Data readData(Packet packet, Packet.Reader in) throws IOException {
+        Data data = new Data();
+
         if (packet.atLeast(ProtocolVersion.v1_16)) {
             in.readBoolean(); // unknown
         }
@@ -103,17 +115,17 @@ public class PacketUpdateLight {
                 throw new IOException("Expected " + skyLightMask.cardinality() + " sky light sections but got " + skyLightsSent);
             }
         }
-        this.skyLight = new ArrayList<>(skySections);
+        data.skyLight = new ArrayList<>(skySections);
         for (int i = 0; i < skySections; i++) {
             if (skyLightMask.get(i)) {
                 if (in.readVarInt() != 2048) {
                     throw new IOException("Expected sky light byte array to be of length 2048");
                 }
-                this.skyLight.add(in.readBytes(2048)); // 2048 bytes read = 4096 entries
+                data.skyLight.add(in.readBytes(2048)); // 2048 bytes read = 4096 entries
             } else if (emptySkyLightMask.get(i)) {
-                this.skyLight.add(new byte[2048]);
+                data.skyLight.add(new byte[2048]);
             } else {
-                this.skyLight.add(null);
+                data.skyLight.add(null);
             }
         }
 
@@ -123,24 +135,29 @@ public class PacketUpdateLight {
                 throw new IOException("Expected " + blockLightMask.cardinality() + " block light sections but got " + blockLightsSent);
             }
         }
-        this.blockLight = new ArrayList<>(blockSections);
+        data.blockLight = new ArrayList<>(blockSections);
         for (int i = 0; i < blockSections; i++) {
             if (blockLightMask.get(i)) {
                 if (in.readVarInt() != 2048) {
                     throw new IOException("Expected block light byte array to be of length 2048");
                 }
-                this.blockLight.add(in.readBytes(2048)); // 2048 bytes read = 4096 entries
+                data.blockLight.add(in.readBytes(2048)); // 2048 bytes read = 4096 entries
             } else if (emptyBlockLightMask.get(i)) {
-                this.blockLight.add(new byte[2048]);
+                data.blockLight.add(new byte[2048]);
             } else {
-                this.blockLight.add(null);
+                data.blockLight.add(null);
             }
         }
+
+        return data;
     }
 
     private void write(Packet packet, Packet.Writer out) throws IOException {
         out.writeVarInt(this.x);
         out.writeVarInt(this.z);
+    }
+
+    static void writeData(Packet packet, Packet.Writer out, Data data) throws IOException {
         if (packet.atLeast(ProtocolVersion.v1_16)) {
             out.writeBoolean(true); // unknown, ViaVersion always writes true, so we'll do so as well
         }
@@ -152,8 +169,8 @@ public class PacketUpdateLight {
         List<byte[]> skyLights = new ArrayList<>();
         List<byte[]> blockLights = new ArrayList<>();
 
-        for (int i = 0; i < this.skyLight.size(); i++) {
-            byte[] skyLight = this.skyLight.get(i);
+        for (int i = 0; i < data.skyLight.size(); i++) {
+            byte[] skyLight = data.skyLight.get(i);
             if (skyLight != null) {
                 if (Arrays.equals(EMPTY, skyLight)) {
                     emptySkyLightMask.set(i);
@@ -163,8 +180,8 @@ public class PacketUpdateLight {
                 }
             }
         }
-        for (int i = 0; i < this.blockLight.size(); i++) {
-            byte[] blockLight = this.blockLight.get(i);
+        for (int i = 0; i < data.blockLight.size(); i++) {
+            byte[] blockLight = data.blockLight.get(i);
             if (blockLight != null) {
                 if (Arrays.equals(EMPTY, blockLight)) {
                     emptyBlockLightMask.set(i);
@@ -194,6 +211,19 @@ public class PacketUpdateLight {
         for (byte[] bytes : blockLights) {
             out.writeVarInt(2048); // dunno why Minecraft feels the need to send these
             out.writeBytes(bytes);
+        }
+    }
+
+    public static class Data {
+        public List<byte[]> skyLight;
+        public List<byte[]> blockLight;
+
+        public Data() {
+        }
+
+        public Data(List<byte[]> skyLight, List<byte[]> blockLight) {
+            this.skyLight = skyLight;
+            this.blockLight = blockLight;
         }
     }
 }

build.gradle
@@ -73,7 +73,7 @@ repositories {
 }
 
 dependencies {
-    preshadow 'com.github.replaymod.viaversion:viaversion-common:86a4b8b'
+    preshadow 'com.github.replaymod.viaversion:viaversion-common:7615405'
 
     compile 'com.google.guava:guava:17.0'
     compile 'com.google.code.gson:gson:2.3.1'

src/main/java/com/replaymod/replaystudio/filter/DimensionTracker.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2021
+ *
+ * This file is part of ReplayStudio.
+ *
+ * ReplayStudio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ReplayStudio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ReplayStudio.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.replaymod.replaystudio.filter;
+
+import com.google.gson.JsonObject;
+import com.replaymod.replaystudio.PacketData;
+import com.replaymod.replaystudio.Studio;
+import com.replaymod.replaystudio.protocol.Packet;
+import com.replaymod.replaystudio.protocol.PacketType;
+import com.replaymod.replaystudio.protocol.packets.PacketJoinGame;
+import com.replaymod.replaystudio.protocol.packets.PacketRespawn;
+import com.replaymod.replaystudio.protocol.registry.DimensionType;
+import com.replaymod.replaystudio.stream.PacketStream;
+
+import java.io.IOException;
+
+public class DimensionTracker implements StreamFilter {
+
+    public String dimension;
+    public DimensionType dimensionType;
+
+    @Override
+    public void onStart(PacketStream stream) {
+    }
+
+    @Override
+    public boolean onPacket(PacketStream stream, PacketData data) throws IOException {
+        Packet packet = data.getPacket();
+        PacketType type = packet.getType();
+
+        switch (type) {
+            case Respawn: {
+                PacketRespawn packetRespawn = PacketRespawn.read(packet);
+                dimension = packetRespawn.dimension;
+                dimensionType = packetRespawn.dimensionType;
+                break;
+            }
+            case JoinGame: {
+                PacketJoinGame packetJoinGame = PacketJoinGame.read(packet);
+                dimension = packetJoinGame.dimension;
+                dimensionType = packetJoinGame.dimensionType;
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onEnd(PacketStream stream, long timestamp) throws IOException {
+    }
+
+    @Override
+    public String getName() {
+        return "track_dimension";
+    }
+
+    @Override
+    public void init(Studio studio, JsonObject config) {
+    }
+}

src/main/java/com/replaymod/replaystudio/filter/SquashFilter.java
@@ -39,6 +39,7 @@ import com.replaymod.replaystudio.protocol.packets.PacketSetSlot;
 import com.replaymod.replaystudio.protocol.packets.PacketTeam;
 import com.replaymod.replaystudio.protocol.packets.PacketUpdateLight;
 import com.replaymod.replaystudio.protocol.packets.PacketWindowItems;
+import com.replaymod.replaystudio.protocol.registry.DimensionType;
 import com.replaymod.replaystudio.stream.IteratorStream;
 import com.replaymod.replaystudio.stream.PacketStream;
 import com.replaymod.replaystudio.util.DPosition;
@@ -171,8 +172,22 @@ public class SquashFilter implements StreamFilter {
      */
     private String dimension;
 
+    /**
+     * Starting with 1.18, we need to know the height of our dimension to be able to parse a chunk packet.
+     */
+    private DimensionType dimensionType;
+
+    public SquashFilter(String dimension, DimensionType dimensionType) {
+        this.dimension = dimension;
+        this.dimensionType = dimensionType;
+    }
+
+    public SquashFilter(DimensionTracker dimensionTracker) {
+        this(dimensionTracker.dimension, dimensionTracker.dimensionType);
+    }
+
     public SquashFilter copy() {
-        SquashFilter copy = new SquashFilter();
+        SquashFilter copy = new SquashFilter(this.dimension, this.dimensionType);
         copy.registry = this.registry;
         copy.forgeHandshake = this.forgeHandshake;
         this.teams.forEach((key, value) -> copy.teams.put(key, value.copy()));
@@ -187,7 +202,6 @@ public class SquashFilter implements StreamFilter {
         this.latestOnly.forEach((key, value) -> copy.latestOnly.put(key, value.copy()));
         this.chunks.forEach((key, value) -> copy.chunks.put(key, value.copy()));
         copy.unloadedChunks.putAll(this.unloadedChunks);
-        copy.dimension = this.dimension;
         return copy;
     }
 
@@ -297,7 +311,8 @@ public class SquashFilter implements StreamFilter {
             case SpawnParticle:
                 break;
             case Respawn: {
-                String newDimension = PacketRespawn.read(packet).dimension;
+                PacketRespawn packetRespawn = PacketRespawn.read(packet);
+                String newDimension = packetRespawn.dimension;
                 if (dimension == null) {
                     // We do not know which dimension we are current in, so we cannot know how to handle this packet.
                     // Instead we flush all state accumulated so far, and then start fresh with the newly
@@ -314,6 +329,7 @@ public class SquashFilter implements StreamFilter {
                     entities.clear();
                 }
                 dimension = newDimension;
+                dimensionType = packetRespawn.dimensionType;
 
                 PacketData prev = this.latestOnly.put(type, data.retain());
                 if (prev != null) {
@@ -330,7 +346,9 @@ public class SquashFilter implements StreamFilter {
                 currentWindow.clear();
                 entities.values().forEach(Entity::release);
                 entities.clear();
-                dimension = PacketJoinGame.read(packet).dimension;
+                PacketJoinGame packetJoinGame = PacketJoinGame.read(packet);
+                dimension = packetJoinGame.dimension;
+                dimensionType = packetJoinGame.dimensionType;
                 forgeHandshake = false;
             case SetExperience:
             case PlayerAbilities:
@@ -348,11 +366,11 @@ public class SquashFilter implements StreamFilter {
                 chunks.computeIfAbsent(
                         ColumnPos.coordToLong(updateLight.getX(), updateLight.getZ()),
                         idx -> new ChunkData(data.getTime(), updateLight.getX(), updateLight.getZ())
-                ).updateLight(updateLight);
+                ).updateLight(updateLight.getData());
                 break;
             case ChunkData:
             case UnloadChunk:
-                PacketChunkData chunkData = PacketChunkData.read(packet);
+                PacketChunkData chunkData = PacketChunkData.read(packet, dimensionType.getSections());
                 if (chunkData.isUnload()) {
                     unloadChunk(data.getTime(), chunkData.getUnloadX(), chunkData.getUnloadZ());
                 } else {
@@ -580,8 +598,14 @@ public class SquashFilter implements StreamFilter {
         }
 
         for (ChunkData chunk : chunks.values()) {
+            PacketUpdateLight.Data lightData = new PacketUpdateLight.Data(
+                    Arrays.asList(chunk.skyLight),
+                    Arrays.asList(chunk.blockLight)
+            );
             Column column = new Column(
-                    chunk.x, chunk.z, chunk.changes, chunk.biomeData, chunk.tileEntities, chunk.heightmaps, chunk.biomes, chunk.useExistingLightData
+                    chunk.x, chunk.z, chunk.changes,
+                    chunk.biomeData, chunk.tileEntities, chunk.heightmaps, chunk.biomes, chunk.useExistingLightData,
+                    lightData
             );
             if (column.isFull() || !Utils.containsOnlyNull(chunk.changes)) {
                 result.add(new PacketData(chunk.firstAppearance, PacketChunkData.load(column).write(registry)));
@@ -596,9 +620,9 @@ public class SquashFilter implements StreamFilter {
             for (MutablePair<Long, PacketBlockChange> pair : chunk.allBlockChanges.values()) {
                 result.add(new PacketData(pair.getLeft(), pair.getRight().write(registry)));
             }
-            if (chunk.hasLight()) {
-                result.add(new PacketData(chunk.firstAppearance, new PacketUpdateLight(
-                        chunk.x, chunk.z, Arrays.asList(chunk.skyLight), Arrays.asList(chunk.blockLight)).write(registry)));
+            if (chunk.hasLight() && registry.olderThan(ProtocolVersion.v1_18)) {
+                result.add(new PacketData(chunk.firstAppearance,
+                        new PacketUpdateLight(chunk.x, chunk.z, lightData).write(registry)));
             }
         }
         chunks.clear();
@@ -676,6 +700,9 @@ public class SquashFilter implements StreamFilter {
                 column.biomes,
                 column.useExistingLightData
         );
+        if (column.lightData != null) { // 1.18+
+            chunk.updateLight(column.lightData);
+        }
     }
 
     private class ChunkData {
@@ -691,7 +718,7 @@ public class SquashFilter implements StreamFilter {
         private Map<Short, MutablePair<Long, PacketBlockChange>>[] blockChanges = new Map[16]; // pre 1.17
         private final Map<Integer, MutablePair<Long, PacketBlockChange>> allBlockChanges = new HashMap<>(); // 1.17+
         // 1.9+
-        private CompoundTag[] tileEntities;
+        private PacketChunkData.TileEntity[] tileEntities;
         // 1.14+
         private CompoundTag heightmaps;
         private byte[][] skyLight = new byte[0][];
@@ -734,7 +761,7 @@ public class SquashFilter implements StreamFilter {
         void update(
                 Chunk[] newChunks,
                 byte[] newBiomeData, // pre 1.15
-                CompoundTag[] newTileEntities, // 1.9+
+                PacketChunkData.TileEntity[] newTileEntities, // 1.9+
                 CompoundTag newHeightmaps, // 1.14+
                 int[] newBiomes, // 1.15+
                 boolean useExistingLightData // 1.16+
@@ -769,9 +796,9 @@ public class SquashFilter implements StreamFilter {
             }
         }
 
-        private void updateLight(PacketUpdateLight packet) { // 1.14+
-            List<byte[]> skyLightUpdates = packet.getSkyLight();
-            List<byte[]> blockLightUpdates = packet.getBlockLight();
+        private void updateLight(PacketUpdateLight.Data data) { // 1.14+
+            List<byte[]> skyLightUpdates = data.skyLight;
+            List<byte[]> blockLightUpdates = data.blockLight;
 
             if (skyLight.length < skyLightUpdates.size()) {
                 skyLight = Arrays.copyOf(skyLight, skyLightUpdates.size());

src/main/java/com/replaymod/replaystudio/io/ReplayInputStream.java
@@ -101,7 +101,7 @@ public class ReplayInputStream extends InputStream {
             this.registry = PacketTypeRegistry.get(registry.getVersion(), State.LOGIN);
         }
         this.in = in;
-        this.viaVersionConverter = ViaVersionPacketConverter.createForFileVersion(fileFormatVersion, fileProtocol, registry.getVersion().getVersion());
+        this.viaVersionConverter = ViaVersionPacketConverter.createForFileVersion(fileFormatVersion, fileProtocol, registry.getVersion().getOriginalVersion());
     }
 
     @Override

src/main/java/com/replaymod/replaystudio/io/ReplayOutputStream.java
@@ -104,7 +104,7 @@ public class ReplayOutputStream extends OutputStream {
         }
         metaData.setFileFormat("MCPR");
         metaData.setFileFormatVersion(ReplayMetaData.CURRENT_FILE_FORMAT_VERSION);
-        metaData.setProtocolVersion(version.getVersion());
+        metaData.setProtocolVersion(version.getOriginalVersion());
         metaData.setGenerator("ReplayStudio v" + studio.getVersion());
         this.metaData = metaData;
 

src/main/java/com/replaymod/replaystudio/protocol/PacketType.java
@@ -114,6 +114,8 @@ public enum PacketType {
     CombatEnter(ProtocolVersion.v1_17, 0x34),
     CombatEntityDead(ProtocolVersion.v1_17, 0x35),
 	DestroyEntity(ProtocolVersion.v1_17, 0x3a), // removed in 1.17.1, that didn't last long
+
+	UpdateSimulationDistance(ProtocolVersion.v1_18, 0x57),
 	;
 
     private final State state;

src/main/java/com/replaymod/replaystudio/protocol/PacketTypeRegistry.java
@@ -222,7 +222,11 @@ public class PacketTypeRegistry {
                     continue;
                 }
                 AbstractProtocol.ProtocolPacket mapping = entry.getValue();
-                result.add(Pair.of(mapping.getOldID(), mapping.getNewID()));
+                com.replaymod.replaystudio.lib.viaversion.api.protocol.packet.PacketType unmappedPacketType = mapping.getUnmappedPacketType();
+                com.replaymod.replaystudio.lib.viaversion.api.protocol.packet.PacketType mappedPacketType = mapping.getMappedPacketType();
+                int oldId = unmappedPacketType != null ? unmappedPacketType.getId() : -1;
+                int newId = mappedPacketType != null ? mappedPacketType.getId() : -1;
+                result.add(Pair.of(oldId, newId));
             }
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);

commit 6a5cf11948834411b1c5ca780552ce606164eb61

Fix PacketUpdateLight.write not actually writing the data
Bug introduced with a5a92b6.

src/main/java/com/replaymod/replaystudio/protocol/packets/PacketUpdateLight.java
@@ -155,6 +155,7 @@ public class PacketUpdateLight {
     private void write(Packet packet, Packet.Writer out) throws IOException {
         out.writeVarInt(this.x);
         out.writeVarInt(this.z);
+        writeData(packet, out, this.data);
     }
 
     static void writeData(Packet packet, Packet.Writer out, Data data) throws IOException {